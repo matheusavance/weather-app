b28565d877877296ff4df80fe7c40069c1871cbc
Matheus Avance <matheus.avance@gmail.com>
2025-05-13 18:41:30 +0200

    mudança/estilização de componentes e resstruturação da call da api

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+function Logo() {
+  return (
+    <div className="flex flex-col items-center py-6">
+      <div className="flex items-center mb-4">
+        <img src="/logo.png" alt="Logo" className="h-20 mr-2" />
+        <div className="font-bold text-2xl">BlackCelta</div>
+      </div>
+    </div>
+  );
+}
+
+export default Logo;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,30 +1,27 @@
 import { useEffect, useState } from 'react';
 import { getWeatherByCity } from '../services/weatherAPI';
 import WeatherCard from '../components/WeatherCard';
-import SearchBarWithLogo from '../components/SearchBar';
+import SearchBar from '../components/SearchBar';
+import Logo from '../components/Logo';
 
 function Home() {
   const [weather, setWeather] = useState(null);
-  const city = 'Guarapari';
+  const [city, setCity] = useState('');
 
-  useEffect(() => {
-    const fetchWeather = async () => {
-      try {
-        const data = await getWeatherByCity(city);
-        setWeather(data);
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchWeather();
-  }, []);
+  const clickButton = async () => {
+    try {
+      const data = await getWeatherByCity(city);
+      setWeather(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center">
-      <SearchBarWithLogo />
-      {weather ? <WeatherCard data={weather} /> : <p>Carregando...</p>}
+      <Logo />
+      <SearchBar city={city} setCity={setCity} clickButton={clickButton} />
+      {weather ? <WeatherCard data={weather} city={city} /> : <p>Loading...</p>}
     </div>
   );
 }
